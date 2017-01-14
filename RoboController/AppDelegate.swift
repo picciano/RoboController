1104c7d7531d5a1bdc7d6f0fd12c8afc5c4a52d9
Anthony Picciano <anthony.picciano@gmail.com>
2017-01-14 16:40:27 -0700

    Initial Commit

---
+++ RoboController/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RoboController
+//
+//  Created by Anthony Picciano on 1/14/17.
+//  Copyright © 2017 Anthony Picciano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
